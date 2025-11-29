a7d383183c32bbd8a793344ca999127b6a892f0c
allin58 <32274183+allin58@users.noreply.github.com>
2025-11-29 20:00:55 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,27 +5,18 @@ from datetime import datetime
 from functools import wraps
 
 import jwt
-from flask import Flask, request, jsonify, redirect, send_file
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from asgiref.wsgi import WsgiToAsgi
 
-
-
-# ==================== Flask app ====================
 app = Flask(__name__)
-
-# На Vercel в продакшене куки с Secure и SameSite=None работают только по HTTPS
 app.config.update(
     SESSION_COOKIE_SAMESITE='None',
     SESSION_COOKIE_SECURE=True,
-    SECRET_KEY=os.urandom(32)  # лучше генерировать каждый раз в serverless
+    SECRET_KEY=os.urandom(32)
 )
 
-CORS(app, supports_credentials=True, origins=["*"])  # подстрой под свои домены в проде
-
-asgi_app = WsgiToAsgi(app)
-def handler(event, context=None):
-    return asgi_app(event, context or {})
+CORS(app, supports_credentials=True, origins=["*"])
 
 @app.route("/ping")
 def ping():
@@ -33,26 +24,7 @@ def ping():
         "status": "ALIVE",
         "message": "pong",
         "time": datetime.utcnow().isoformat() + "Z",
-        "turso": "disabled for debug"        
+        "debug": "100% clean"
     })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+asgi_app = WsgiToAsgi(app)
